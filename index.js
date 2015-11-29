ae89584c958ee1a16f38b8bd1bf766e29dca738e
Wang Ruqiang <likegun94@gmail.com>
2015-11-29 14:20:45 +0800

    1.优化了代码结构，精简，看起来超舒服了。 2.添加了一些数学组合的方法，以后可以当自己的库了。

--- index.js
+++ index.js
@@ -2,50 +2,19 @@ var repl = require('repl');
 var r = repl.start('> ');
 var c = r.context;
 var Cal24 = require('./lib/cal24.js');
-
-var cal24;
-
-//生成游戏数字
-function getGameNumber() {
-	//因为随机生成的4个数字可能是没有解的，所以要一直找到一个有解的数字组合
-	while (true) {
-		var gameNumber = generateNumber();
-		if (cal24)
-			cal24.clear();
-		return gameNumber;
-	}
-}
-
-/**
- * 生成一个整型数组，长度为4，取值范围为1~13
- * @return {Array of Int}
- */
-function generateNumber() {
-	var i = 4,
-		numberArray = [];
-	while (i-- > 0) {
-		//保证1、2...12、13等可能出现
-		numberArray.push(Math.floor(parseInt(Math.random() * 12.9999999)) + 1)
-	}
-	return numberArray;
-}
+var cal24 = new Cal24();
 
 r.commands['new'] = {
 	help: "新游戏",
 	action: function () {
-		var gameNumber = getGameNumber();
-		cal24 = new Cal24(gameNumber);
-		if (cal24.getTracks().length !== 0) {
-			console.log("游戏数字是:" + gameNumber)
-		}
+		cal24.newGame();
 	}
 };
 
 r.commands['answer'] = {
 	help: "查看答案",
 	action: function () {
-		console.log("有这些算法:");
-		console.log(cal24.getTracks());
+		console.log(cal24.getResults());
 	}
 };
 
@@ -64,10 +33,6 @@ r.commands['cal'] = {
 		if (numberArray.length !== 4) {
 			return console.log('必须输入四个数字');
 		}
-		if (cal24)
-			cal24.clear();
-		cal24 = new Cal24(numberArray);
-		console.log("有这些算法:");
-		console.log(cal24.getTracks());
+		cal24.calByNumberArr(numberArray);
 	}
 };
